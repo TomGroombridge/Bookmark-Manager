850294a3485038f8aa68f0d30b83700e308c1106
TomGroombridge <groombridgetom@gmail.com>
2013-12-17 13:42:59 +0000

    extra tags added and working

--- app/model/link.rb
+++ app/model/link.rb
@@ -6,6 +6,8 @@ class Link
 	#this makes the instance of this class Datamapper resources
 	include DataMapper::Resource
 
+	has n, :tags, :through => Resource
+
 	#This block describes what resources our model will have
 	property :id, Serial #Serail means that it will be auto-incremented for every record
 	property  :title, String
--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/model/link' #this needs to be done after datamapper is initialised
-
+require './app/model/tag'
 #After decalring your models, you should finalise them 
 DataMapper.finalize
 
@@ -21,6 +21,16 @@ end
 post '/links' do 
 	url = params['url']
 	title = params['title']
-	Link.create(:url => url, :title => title)
+	tags = params["tags"].split(" ").map do |tag|
+		#this will either find this tag or create it if it doesnt exist already
+		Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to('/')
+end
+
+get '/tags/:text' do
+  tag = Tag.first(:text => params[:text])
+  @links = tag ? tag.links : []
+  erb :index
 end
