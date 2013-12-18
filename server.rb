a9091a7621b80c5b2ed2e69cdfc44f800e744cf0
TomGroombridge <groombridgetom@gmail.com>
2013-12-18 17:18:55 +0000

    user authenticate method added

--- server.rb
+++ server.rb
@@ -70,6 +70,22 @@ post '/users' do
 	end
 end
 
+get '/sessions/new' do
+	erb:"sessions/new"
+end
+
+post '/sessions' do 
+	email, password = params[:email], params[:password]
+	user = User.authenticate(email, password)
+	if user
+		session[:user_id] = user.id
+		redirect to ('/')
+	else
+		flash[:errors] = ["This email or password are incorrect"]
+		erb :"sessions/new"
+	end	
+end
+
 
 
 
